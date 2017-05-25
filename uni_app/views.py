2f3e5e66765468492d95e26a9e729440483960eb
Akshay <akshay@nimbrisk.com>
2017-05-25 13:13:57 +0530

    unimax web set up

---
+++ uni_app/views.py
@@ -0,0 +1,14 @@
+from django.http import HttpResponseRedirect, HttpResponse
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+from django.contrib.auth.decorators import (
+    login_required, user_passes_test
+)
+from django.conf import settings
+import requests
+import json
+
+
+        	
+def index(request):
+	return render_to_response("index.html",locals(),RequestContext(request))
